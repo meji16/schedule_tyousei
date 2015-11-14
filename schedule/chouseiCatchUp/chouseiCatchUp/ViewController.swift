48e60c31b0c0d8d0cca4e2d4d19310e256c2f05e
Takuma Kaito <Takuma_Kaito@KaitoTakuma-no-MacBook.local>
2015-11-15 07:19:20 +0900

    さっきのコミットしたやつPushで失敗したので(ローカルの違うフォルダにプロジェクト作ってしまった)再度挑戦。

---
+++ schedule/chouseiCatchUp/chouseiCatchUp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  chouseiCatchUp
+//
+//  Created by Takuma Kaito on 2015/11/15.
+//  Copyright (c) 2015年 Kaito Yato. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
